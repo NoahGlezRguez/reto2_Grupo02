a844bad9282a408ffbffb8d60bc9cfe38c2e4b86
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-20 13:39:28 +0100

    new: metodo mostrar bienvenida
    la pantalla de inicio

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/vista/MostrarMsg.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/vista/MostrarMsg.java
@@ -2,4 +2,22 @@ package vista;
 
 public class MostrarMsg {
 
+	public static void bienvenida() {
+		System.out.println("***********************************************************************");
+        System.out.println("*                                                                     *");
+        System.out.println("*   ###############################################################   *");
+        System.out.println("*   #                                                             #   *");
+        System.out.println("*   #   ★☆★         ¡BIENVENIDX A CINES ELORRIETA!	     ★☆★    #   *");
+        System.out.println("*   #                                                             #   *");
+        System.out.println("*   #                Donde experimentar nuevos mundos...	        #   *");
+        System.out.println("*   #              desde la más cómoda de las butacas:.           #   *");
+        System.out.println("*   #                                                             #   *");
+        System.out.println("*   ###############################################################   *");
+        System.out.println("*                                                                     *");
+        System.out.println("*            [🎥]  [🎞️]  [🎬]  [🎞️]  [📽️]  [🎞️]  [🎦]  [📽️]             *");
+        System.out.println("*                                                                     *");
+        System.out.println("*              → Pulsa la tecla Enter para comenzar ←                 *");
+        System.out.println("*                                                                     *");
+        System.out.println("***********************************************************************");
+	}
 }
