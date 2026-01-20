37b14bb415c27cc49244ae5487b75f05839b89bd
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-20 21:14:06 +0100

    doc

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/Main.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/Main.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 import vista.Menu;
 import vista.MostrarMsg;
 
-
+/**
+ * Clase principal del programa que gestiona el flujo más básico de la venta de entradas del cine.
+ */
 public class Main {
 
 	public static String 	entrada = "";
 	public static Scanner 	teclado = new Scanner(System.in);
 	public static boolean	reiniciar = true;
 
+	/**
+	 * Método base, que maneja el bucle y reinicia o cierra el programa cuando se indica.
+	 * Solo muestra la pantalla de bienvenida la primera vez o cuando se indica que se reinicia.
+	 * Cuando se da a la opcion de salir, ofrece una confirmacion.
+	 * Al salir del programa muestra una pantalla de despedida.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		int		opc = 0;
