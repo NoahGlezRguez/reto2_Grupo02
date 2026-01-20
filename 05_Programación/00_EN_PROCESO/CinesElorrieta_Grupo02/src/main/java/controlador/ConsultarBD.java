994178d9b1a547270043421f989d7a34066eb33e
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-20 16:26:48 +0100

    new: metodos para probar uso de bd en programa

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ConsultarBD.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ConsultarBD.java
@@ -1,5 +1,62 @@
 package controlador;
 
+import java.sql.*;
+
 public class ConsultarBD {
 
+	private static String	rutaBD = "jdbc:mysql://localhost:33060/cine_elorrieta";//ajustar a la ruta real 
+	private static String	user = "DAM_v";
+	private static String	pw = "Elorrieta9753$";
+	
+	public static Connection getConect() {
+		
+		Connection	conexion = null;
+		
+		try {
+			if (conexion == null || conexion.isClosed())
+				conexion = DriverManager.getConnection(rutaBD, user, pw);
+		
+		} catch(SQLException excpsql) {
+			System.out.println("Error, no se pudo realizar la conexión con la base de datos.\n");
+			System.out.println("SQLException: " + excpsql.getMessage());
+	        System.out.println("SQLState: " + excpsql.getSQLState());
+	        System.out.println("VendorError: " + excpsql.getErrorCode());
+		}
+		return(conexion);
+	}
+
+	public static void id_pelis_consulta() { //metodo de prueba
+		
+		int	pelis_ids[] = null;
+		int	numPelis = 0;
+		
+		Connection conexion = ConsultarBD.getConect();
+		Statement	sentencia = null;
+		ResultSet 	result = null;
+		try {
+			sentencia = conexion.createStatement();
+			result = sentencia.executeQuery("Select * from Pelicula"); //ajustar a lo que necesitemos
+			while(result.next())
+				numPelis++;
+			pelis_ids = new int[numPelis];
+			result = sentencia.executeQuery("Select * from Pelicula");
+			for(int i = 0; i < numPelis; i++)
+			{
+				if (result.next())
+					pelis_ids[i] = Integer.parseInt(result.getString("IDPeli"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		for (int i = 0; i < numPelis; i++) {
+			System.out.printf("\t- item %d -> id de peli = %d.\n", i, pelis_ids[i]);
+		}
+		try {
+			conexion.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/Main.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/Main.java
@@ -1,36 +1,16 @@
 package controlador;
 
 import java.util.Scanner;
-import java.sql.*;
+
 
 public class Main {
 
 	public static String 	entrada;
 	public static Scanner 	teclado = new Scanner(System.in);
-	public static String	rutaBD = "jdbc:mysql://10.5.6.116:33080/CinesElorrieta";//ajustar a la ruta real 
-	public static String	user = "admin";
-	public static String	pw = "CinesElorrieta123";
-	
-	
-	
+
 	public static void main(String[] args) {
 		
-		Connection	conexion = null;
-		Statement	sentencia = null;
-		ResultSet 	result = null;
-		
-		try {
-			conexion = DriverManager.getConnection(rutaBD, user, pw);
-			
-			sentencia = conexion.createStatement();
-			
-			result = sentencia.executeQuery("Select * from tablaEjemplo"); //ajustar a lo que necesitemos
-			
-			//recorremos los resultados
-			System.out.printf("Mostrando los resultados:", result);
-		} catch(SQLException excpsql) {
-			System.out.println("Error, no se pudo realizar la conexión con la base de datos");
-		}
-	}
+		ConsultarBD.id_pelis_consulta();//testeo de metodos de bd
 
+	}
 }
