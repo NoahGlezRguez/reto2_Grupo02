f60c6fd7208d4403c0af3b2b33aacbc574a44711
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-20 21:04:14 +0100

    add: opcion de reiniciar el programa
    una variable estatica booleana permite que desde cualquier parte del programa se pueda indicar que se desea empezar de cero, es decir, a la pantalla de inicio.
    mas adelante, se tendra que implementar mas cosas a parte de mostrar la pantalla de bienvenida (vaciar las variables y setear todo a null como si efectivamente se empezara de nuevo

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/Main.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 	public static String 	entrada = "";
 	public static Scanner 	teclado = new Scanner(System.in);
+	public static boolean	reiniciar = true;
 
 	public static void main(String[] args) {
 		
@@ -18,11 +19,16 @@ public class Main {
 		boolean	apagar = false;
 		
 		while (!apagar) {
-			MostrarMsg.bienvenida();
-			teclado.nextLine();
+			if (reiniciar) {
+				MostrarMsg.bienvenida();
+				teclado.nextLine();
+				reiniciar = false;
+			}
 			opc = Menu.opciones("Operaciones disponibles", operaciones, "Seleccione la operación que desea realizar");
-			if (opc == 0)
-				OperacionesCompra.comprarEntradas();
+			if (opc == 0) {
+				reiniciar = OperacionesCompra.comprarEntradas();
+				
+			}
 			else {
 				if (Menu.siNo("¿Está segurx de que quiere apagar esta máquina?") == 0)
 					apagar = true;
