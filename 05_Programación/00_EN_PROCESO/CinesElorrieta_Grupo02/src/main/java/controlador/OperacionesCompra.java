244d0b36ec41602ea0afe53009d92f4a5f5576bd
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-20 21:21:24 +0100

    doc

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/OperacionesCompra.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/OperacionesCompra.java
@@ -3,8 +3,18 @@ package controlador;
 import modelo.*;
 import vista.*;
 
+/**
+ * Esta clase contiene las distintas operaciones dentro del proceso de compra. Es el corazón del programa.
+ */
 public class OperacionesCompra {
 
+	/**
+	 * Este método gestiona el flujo del programa cuando el usuario selecciona en el menú previo "Comprar entradas".
+	 * Muestra los diferentes menús, recoge las selecciones realizadas por el usuario y si se cancela o se termina el proceso,
+	 * regresa al menú anterior. 
+	 * 
+	 * @return false en caso de que haya terminado adecuadamente, y true si se ha cancelado el proceso de compra en algún punto.
+	 */
 	public static boolean comprarEntradas() {
 		Pelicula	cartelera[];
 		Pelicula	peliculaElegida = null;
