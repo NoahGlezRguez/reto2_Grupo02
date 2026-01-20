e64d676a490d1bb877718981ce7243c06eae95bf
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-20 17:34:02 +0100

    new: menu inicial en Main

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/Main.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/Main.java
@@ -2,15 +2,33 @@ package controlador;
 
 import java.util.Scanner;
 
+import vista.Menu;
+import vista.MostrarMsg;
+
 
 public class Main {
 
-	public static String 	entrada;
+	public static String 	entrada = "";
 	public static Scanner 	teclado = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
-		ConsultarBD.id_pelis_consulta();//testeo de metodos de bd
-
+		int		opc = 0;
+		String	operaciones[] = {"Comprar entradas", "Salir y apagar esta máquina"};
+		boolean	apagar = false;
+		
+		while (!apagar) {
+			MostrarMsg.bienvenida();
+			teclado.nextLine();
+			opc = Menu.opciones("Operaciones disponibles", operaciones, "Seleccione la operación que desea realizar");
+			if (opc == 0)
+				OperacionesCompra.comprarEntradas();
+			else {
+				if (Menu.siNo("¿Está segurx de que quiere apagar esta máquina?") == 0)
+					apagar = true;
+			}
+				
+		}
+		MostrarMsg.despedida();
 	}
 }
