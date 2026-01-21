db48bfb1b7a4ff0380f1b7f05a596b720cc35e32
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-21 21:21:45 +0100

    useless: comentario sobre si borrar clase o no

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/modelo/Cartelera.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/modelo/Cartelera.java
@@ -1,6 +1,6 @@
 package modelo;
 
-public class Cartelera {
+public class Cartelera {//CREO QUE ESTA CLASE SOBRA Y HABRIA QUE BORRARLA
 
 	String 		ahora = "current_timestamp()";
 	Pelicula 	carteleraActualizada[];
