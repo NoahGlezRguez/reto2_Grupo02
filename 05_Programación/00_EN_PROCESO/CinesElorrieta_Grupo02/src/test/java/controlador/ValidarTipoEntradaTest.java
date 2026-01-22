c21b82bf19bc2df9c68183d4914a2fe678c238da
Rodny <rodnybryan.guerrerode@elorrieta-errekamari.com>
2026-01-22 12:04:47 +0000

    add: test - validarTipoEntrada

---
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/test/java/controlador/ValidarTipoEntradaTest.java
@@ -0,0 +1,29 @@
+package controlador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValidarTipoEntradaTest {
+
+	@Test
+	public void testCheckSoloLetras() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckSoloAlfanumerico() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckSoloNumeroDecimal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckSoloNumeroEntero() {
+		fail("Not yet implemented");
+	}
+
+}
