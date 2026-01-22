c638092f528f172a0d71275f4b6e20639977125f
Rodny <rodnybryan.guerrerode@elorrieta-errekamari.com>
2026-01-22 10:27:02 +0000

    wip: test de prueba Validarlogin

---
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ValidarLoginTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package controlador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * 
+ */
+public class ValidarLoginTest {
+
+	/**
+	 * Metodo test para {@link controlador.ValidarLogin#validar(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testValidar() {
+		fail("Not yet implemented");
+	}
+
+}
