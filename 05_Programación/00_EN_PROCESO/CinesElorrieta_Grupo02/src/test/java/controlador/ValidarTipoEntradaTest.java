c26a48e8db11f16dc24c776a00f33ef701b90077
Rodny <rodnybryan.guerrerode@elorrieta-errekamari.com>
2026-01-22 12:27:26 +0000

    add: ValidarTipoEntrada true-false

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/test/java/controlador/ValidarTipoEntradaTest.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/test/java/controlador/ValidarTipoEntradaTest.java
@@ -7,8 +7,17 @@ import org.junit.Test;
 public class ValidarTipoEntradaTest {
 
 	@Test
-	public void testCheckSoloLetras() {
-		fail("Not yet implemented");
+	public void testCheckSoloLetrasTrue() {
+		boolean expectedResult = true;
+		boolean actualResult = ValidarTipoEntrada.checkSoloLetras("nombreUSUARIO");
+		assertTrue(expectedResult == actualResult);
+	}
+	
+	@Test
+	public void testCheckSoloLetrasFalse() {
+		boolean expectedResult = false;
+		boolean actualResult = ValidarTipoEntrada.checkSoloLetras(null);
+		assertTrue(expectedResult == actualResult);
 	}
 
 	@Test
