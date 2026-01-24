bf99b8d282632aacc0fffb0a17e24b869b2d3585
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-24 01:48:48 +0100

    link: ahora este archivo funciona con el resto del programa

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ValidarLogin.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ValidarLogin.java
@@ -1,43 +1,39 @@
-//package controlador;
-//
-//import java.sql.Connection;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//
-//public class ValidarLogin {
-//
-//	public static boolean validar(String usuario, String password) {
-//		Connection conexion = ConsultarBD.getConect();
-//		Statement sentencia = null;
-//		ResultSet r = null;
-//		boolean valido = false;
-//
-//		try {
-//			sentencia = conexion.createStatement();
-//			r = sentencia.executeQuery("select NomCli, userpassword from cliente;");
-//
-//			while (r.next()) {
-//				String u = r.getString("NomCli");
-//				String p = r.getString("userpassword");
-//
-//				if (u.equals(usuario) && p.equals(password)) {
-//					valido = true;
-//					break;
-//				}
-//			}
-//
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//
-//		try {
-//			conexion.close();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//		return valido;
-//	}
-//}
+package controlador;
+
+import java.sql.*;
+
+public class ValidarLogin {
+
+	public static boolean validar(String usuario, String password) {
+		Connection conexion = ConsultarBD.conectarConBD();
+		Statement sentencia = null;
+		ResultSet r = null;
+		boolean valido = false;
+
+		try {
+			sentencia = conexion.createStatement();
+			r = sentencia.executeQuery("select NomCli, userpassword from cliente;");
+
+			while (r.next()) {
+				String u = r.getString("NomCli");
+				String p = r.getString("userpassword");
+
+				if (u.equals(usuario) && p.equals(password)) {
+					valido = true;
+					break;
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			conexion.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return valido;
+	}
+}
