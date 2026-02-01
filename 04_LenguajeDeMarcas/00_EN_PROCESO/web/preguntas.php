c1e4b50beb86947b05e426c82ec688d2b853d05a
gonzalezguaranga-hue <gonzalezguaranga@gmail.com>
2026-02-01 22:25:14 +0100

    update ccs de pagina faq

--- 04_LenguajeDeMarcas/00_EN_PROCESO/web/preguntas.php
+++ 04_LenguajeDeMarcas/00_EN_PROCESO/web/preguntas.php
@@ -6,25 +6,36 @@
     <br> <br> <br>
     <div id="faqdiv">
         <h1 id="preguntasf">Preguntas Frecuentes</h1> <br/>
-        <h3>¿Cuál es nuestro horario?</h3>
-        <p>Nuestro horario es de 15:00 a 00:00 horas, todos los días.</p>
-        <br/> <br>
-        <h3>¿Como puedo contactaros/como puedo encontraros?</h3>
-        <p>¡En la pagina de Contacto tienes toda la informacion para contactarnos!</p>
-        <br/> <br>
-        <h3>¿Puedo conseguir descuentos en la pagina web?</h3>
-        <p>¡Claro que si! Toda la informacion relacionada a descuentos se encuentra mas detallada en la pagina de Promociones.</p>
-        <br/> <br>
-        <h3>¿Que le pasa a mi informacion cuando creo una cuenta?</h3>
-        <p>¡En Elorrieta Cines no recopilamos los datos de nuestrxs clientes! ¡Tu informacion personal nunca será usada para nada mas que para identificarte!</p>
-        <br/> <br>
-        <h3>¡Me equivoqué en mi contraseña al crear mi cuenta! ¿Que puedo hacer para solucionarlo?</h3>
-        <p>Contactanos, y un experto se encargará de tu caso.</p>
-        <br/> <br>
-        <h3>¿Como puedo borrar mi cuenta?</h3>
-        <p>¡No puedes! :D</p>
-        <br/> <br>
-        
+
+        <div class="separador">
+            <h3>¿Cuál es nuestro horario?</h3>
+            <p>Nuestro horario es de 15:00 a 00:00 horas, todos los días.</p>
+        </div>
+
+        <div class="separador">
+            <h3>¿Como puedo contactaros/como puedo encontraros?</h3>
+            <p>¡En la pagina de Contacto tienes toda la informacion para contactarnos!</p>
+        </div>
+
+        <div class="separador">
+            <h3>¿Donde estáis ubicados?</h3>
+            <p>¡En la pagina de Contacto tienes toda la informacion sobre nuestra ubicacion!</p>
+        </div>
+
+        <div class="separador">
+            <h3>¿Puedo conseguir descuentos en la pagina web?</h3>
+            <p>¡Claro que si! Toda la informacion relacionada a descuentos se encuentra mas detallada en la pagina de Promociones.</p>
+        </div>
+
+        <div class="separador">
+            <h3>¿Que le pasa a mi informacion cuando creo una cuenta?</h3>
+            <p>¡En Elorrieta Cines no recopilamos los datos de nuestrxs clientes! ¡Tu informacion personal nunca será usada para nada mas que para identificarte!</p>
+        </div>
+        <div class="separador">
+            <h3>¡Me equivoqué en mi contraseña al crear mi cuenta! ¿Que puedo hacer para solucionarlo?</h3>
+            <p>Contactanos, y un experto se encargará de tu caso.</p>
+        </div>
+    
         <br>
     </div>
   
