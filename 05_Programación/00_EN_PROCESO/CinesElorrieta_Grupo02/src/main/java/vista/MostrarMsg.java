9048fb12118a6bc474bd5c39714254094e2bbef5
Rodny <rodnybryan.guerrerode@elorrieta-errekamari.com>
2026-02-02 23:33:44 +0000

    fix: mover factura a la clase compra

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/vista/MostrarMsg.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/vista/MostrarMsg.java
@@ -142,54 +142,6 @@ public class MostrarMsg {
 		
 		return msg[num];
 	}
-	/**
-	 * este método recibe por parámetros en el siguiente orden:</br>
-	 * <ol>
-	 * <li>String idcompra</li>
-	 * <li>String Fecha</li>
-	 * <li>String Plataforma de compra</li>
-	 * <li>String nombre del cliente</li>
-	 * <li>String DNI</li>
-	 * <li>String Descuento</li>
-	 * <li>String importe</li>
-	 * <li>String total</li>
-	 * <li>ArrayList de entradas</li>
-	 * </ol>
-	 * <p> los rellena en un string con fromato y devulve la
-	 * factura a imprimir</p>
-	 * @param 8 String + 1 arraylist
-	 * @return String con formato
-	 */
-	public static String factura(String a, String b, String c, String d, String e, String f, String g, String h, ArrayList<Entrada> entrada) {
-		
-		String hola = "";
-		
-		for(int i = 0; i<entrada.size(); i++) {
-			hola +=  entrada.get(i).toString();
-		};
-		
-		String formato = 
-				"""
-				------------------------------------
-				Compra nº:			%-15s
-				Fecha:				%-15s
-				Plataforma:			%15s
-				Cliente:			%15s
-				DNI:				%15s
-				
-				%s
-				
-				
-				Descuento:			%15s
-				Importe:			%15s
-				
-				
-				Total:				%15s
-				-------------------------------------
-				""".formatted(a, b, c, d, e, hola, f, g, h) ;
-		
-		return formato;
-	}
 	
 	
 }
