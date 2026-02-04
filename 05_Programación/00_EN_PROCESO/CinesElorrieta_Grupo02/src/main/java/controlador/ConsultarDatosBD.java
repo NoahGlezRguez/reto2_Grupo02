b6ec317044b115cffc225f98fc0a5aa22f065ba7
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-02-04 21:14:36 +0100

    javadoc consultarBD ok

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ConsultarDatosBD.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ConsultarDatosBD.java
@@ -7,12 +7,22 @@ import java.util.ArrayList;
 import modelo.*;
 import vista.*;
 
+/**
+ * Esta clase tiene los métodos necesarios para consultar datos de la base de datos del cine, incluido
+ * el que realiza la conexión con la base de datos, necesaria para realizar las consultas. 
+ */
 public class ConsultarDatosBD {
 
 	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
 	 * conectar con la bbdd
 	 * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 	
+	/**
+	 * Este método llama al método datos para obtener la información necesaria para
+	 * realizar una conexión con la base de datos del cine. Si hubiera algún error
+	 * en el proceso, lo mostrará en la terminal.
+	 * @return Devuelve la conexión si se ha realizado con éxito, null en caso contrario.
+	 */
 	public static Connection conectarConBD() {
 		Connection	conexion = null;
 		String data[] = datos();
@@ -29,7 +39,7 @@ public class ConsultarDatosBD {
 	}
 	
 	/**
-	 * <p>este método lee los datos de el fichero <b>ipConfig.txt</b>
+	 * <p>Este método lee los datos del fichero <b>ipConfig.txt</b>
 	 * y los guarda en un array de la siguiente forma:</p></br>
 	 * <ol start="0">
 	 * <li>jdbc:mysql://ip:puerto/DatabaseName</li>
@@ -37,11 +47,13 @@ public class ConsultarDatosBD {
 	 * <li>password</li>
 	 * </ol>
 	 * 
-	 * para modificar los datos se debe actualizar solo el contenido que está
-	 * después de '='</br>
+	 * Para modificar los datos se debe actualizar solo el contenido que está
+	 * después de '=' en ese fichero. El formato de estos datos es esencial
+	 * para que funcione, no debería contener nada más, ni un salto de línea
+	 * extra. De contener algo más, no podrá realizarse la conexión.</br>
 	 *  
-	 * @return <b>String array </b></br>se utilizarán los index del array tal como
-	 * se indica en la lista
+	 * @return <b>String array </b> contiene la ruta, el usuario y la password
+	 * necesarios para la conexión con la base de datos
 	 */
 	private static String [] datos(){
 		
@@ -85,9 +97,15 @@ public class ConsultarDatosBD {
 	 * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 	
 	
-	
-	// consultar y mostrar cartelera sin volcar los datos en memoria, solo se
-	// guardan los idPelis
+	/**
+	 * Este método conecta con la base de datos y consulta cuáles son las 
+	 * películas que tienen sesiones a partir de la fecha actual en adelante.
+	 * Además, muestra los datos respectivos de cada película mostrada.
+	 * Muestra en bucle y con formato cada película que cumple con esa premisa.
+	 * Al mismo tiempo, almacena los ID de las películas mostradas para posteriores
+	 * consultas.
+	 * @return ArrayList de los ID de las películas disponibles en cartelera
+	 */
 	public static ArrayList<Integer> consultarCartelera() {
 
 		String consulta = """
@@ -139,9 +157,16 @@ public class ConsultarDatosBD {
 		return (idPelis);
 	}
 
-
-
-	// consultar, mostrar y guardar fechas de la feli seleccionada
+	
+	/**
+	 * Recibe una película, y usando su ID se consulta en la base de datos
+	 * cuáles son las fechas disponibles que tengas sesiones de ahora
+	 * en adelante. Muestra con formato los resultados y la opcion de volver
+	 * atras.
+	 * @param peliculaElegida - objeto con los datos de la pelicula elegida por el usuario
+	 * @return ArrayList de string donde cada elemento es una fecha disponible de esa
+	 * pelicula
+	 */
 	public static ArrayList<String> consultarFechas(Pelicula peliculaElegida) {
 
 		ArrayList<String> fechas = new ArrayList<>();
@@ -193,6 +218,14 @@ public class ConsultarDatosBD {
 
 	// consultar, mostrar (y guardar solo los idSesiones) una sesion de una pelicula
 	// determinada un dia determinado
+	
+	/**
+	 * Consulta las sesiones disponibles de la pelicula y la fecha
+	 * indicadas, devuelve los ID de las sesiones resultantes.
+	 * @param peliculaElegida - seleccionada previamente por el usuario de la cartelera
+	 * @param fechaElegida - seleccionada previamente por el usuario del listado mostrado
+	 * @return ArrayList de todos los ID de las sesiones consultadas que están disponibles
+	 */
 	public static ArrayList<Integer> consultarSesiones(Pelicula peliculaElegida, String fechaElegida) {// esto tiene
 
 		ArrayList<Integer> sesionesPelicula = new ArrayList<>();
@@ -239,6 +272,15 @@ public class ConsultarDatosBD {
 		return (sesionesPelicula);
 	}
 
+	/**
+	 * Consulta qué sesiones tienen aforo de aquellas sesiones que son de la pelicula y
+	 * fecha indicadas. Muestra el resultado en pantalla con formato y devuelve un 
+	 * arraylist con los id de las sesiones resultantes.
+	 * @param peliculaElegida - elegida por el usuario
+	 * @param fechaElegida - elegida por el usuario
+	 * @param compra - objeto con la informacion del proceso actual de compra
+	 * @return id de las sesiones con aforo disponibles ese dia de esa pelicula
+	 */
 	public static ArrayList<Integer> consultarSesionesConAforoDisponible(Pelicula peliculaElegida,
 			String fechaElegida, Compra compra) {
 
@@ -255,6 +297,7 @@ public class ConsultarDatosBD {
 		// con aforo o sin aforo:
 		sesionesPelicula = consultarSesiones(peliculaElegida, fechaElegida);
 
+		//analizar el aforo
 		for (int i = 0; i < sesionesPelicula.size(); i++) {
 			if (consultarAforo(sesionesPelicula.get(i), compra) > 0) {
 				sesionesConAforo.add(sesionesPelicula.get(i));
@@ -305,6 +348,13 @@ public class ConsultarDatosBD {
 		return (sesionesConAforo);
 	}
 
+	/**
+	 * Analiza el aforo disponible real y actualizado de una sesion concreta
+	 * teniendo en cuenta las entradas de la base de datos y las del proceso de compra
+	 * @param idSesion - la sesion de la que se consulta el aforo
+	 * @param compra - el proceso actual de compra
+	 * @return - el aforo disponible resultante
+	 */
 	public static int consultarAforo(int idSesion, Compra compra) {
 
 		int aforoSala, aforoCompras, aforoCesta, aforoDisponible;
@@ -318,6 +368,12 @@ public class ConsultarDatosBD {
 		return (aforoDisponible);
 	}
 
+	/**
+	 * consulta en la base de datos cual es el aforo ocupado previamente con otras
+	 * compras con entradas para esa sesion y devuelve el resultado
+	 * @param idSesion - la sesion de la que analizar el aforo
+	 * @return - aforo ocupado previamente de esa sesion
+	 */
 	public static int conocerAforoCompras(int idSesion) {
 
 		int aforoOcupado = 0;
@@ -365,6 +421,11 @@ public class ConsultarDatosBD {
 		return (aforoOcupado);
 	}
 
+	/**
+	 * Consulta cual es el aforo de la sala de la sesion indicada
+	 * @param idSesion - sesion de la que analizar el aforo de su sala
+	 * @return - aforo de la sala 
+	 */
 	public static int conocerAforoSala(int idSesion) {
 
 		int aforoSala = -1;
