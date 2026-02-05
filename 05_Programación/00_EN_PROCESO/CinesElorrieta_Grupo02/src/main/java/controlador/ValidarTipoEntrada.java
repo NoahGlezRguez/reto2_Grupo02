3fc96cf91d182759ace5e76dfe7103d4d9c4f04e
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-02-05 01:35:33 +0100

    javadoc validarTipoEntrada ok

--- 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ValidarTipoEntrada.java
+++ 05_Programación/00_EN_PROCESO/CinesElorrieta_Grupo02/src/main/java/controlador/ValidarTipoEntrada.java
@@ -6,64 +6,76 @@ import vista.*;
 
 /**
 	 * Clase que gestiona la verificación de la entrada de datos por teclado según la necesidad.
-	 * Tiene métodos para revisar si solo son letras, o es alfanumérico, un nº decimal o entero, etc.
+	 * Tiene métodos para revisar se introduce lo que se pide.
 	 */
 public class ValidarTipoEntrada {
 			
-		/**
-		 * Valida que la entrada contenga únicamente letras,
-		 * permite espacios en blanco y tabulaciones, pero no números ni símbolos.
-		 * 
-		 * @param entrada - String a validar
-		 * @return true si solo contiene letras (y espacios o tabulaciones), false en caso contrario
-		 */
-		public static boolean checkSoloLetras(String entrada) {
+	/**
+	 * Valida que la entrada contenga únicamente letras, aunque
+	 * permite espacios en blanco y tabulaciones, pero no números ni símbolos.
+	 * 
+	 * @param entrada - String a validar
+	 * @return true si solo contiene letras (y espacios o tabulaciones), false en caso contrario
+	 */
+	public static boolean checkSoloLetras(String entrada) {
 			
-			boolean esCorrecto = true;
+		boolean esCorrecto = true;
 
-			if(entrada != null && !entrada.isBlank()) {
-				entrada = entrada.trim();
-				for (int i = 0; i < entrada.length(); i++) {
-					if (!Character.isLetter(entrada.charAt(i)) && entrada.charAt(i) != ' ' && entrada.charAt(i) != '\t') 
-						esCorrecto = false;
-				}
-				if (!esCorrecto)
-					MostrarMsg.errores(2);
-			}
-			else {
-				MostrarMsg.errores(3);
-				esCorrecto = false;
-			}
-			return (esCorrecto);
-		}
-
-			
-		/**
-		 * Analiza un string que recibe por parámetro e indica si este solo contiene dígitos y nada más.
-		 * 
-		 * @param entrada - String a validar
-		 * @return true si es son solo dígitos, false en caso contrario
-		 */
-		public static boolean checkNum(String entrada) {
-			
-			boolean esCorrecto = true;
-			
+		if(entrada != null && !entrada.isBlank()) {
 			entrada = entrada.trim();
-			
-			try {
-				Integer.parseInt(entrada);
-				
-			}catch(Exception e) {
-				esCorrecto = false;
+			for (int i = 0; i < entrada.length(); i++) {
+				if (!Character.isLetter(entrada.charAt(i)) && entrada.charAt(i) != ' ' && entrada.charAt(i) != '\t') 
+					esCorrecto = false;
 			}
+			if (!esCorrecto)
+				MostrarMsg.errores(2);
+		}
+		else {
+			MostrarMsg.errores(3);
+			esCorrecto = false;
+		}
+		return (esCorrecto);
+	}
+
 			
-			return (esCorrecto);
+	/**
+	 * Analiza un string que recibe por parámetro e indica si este solo contiene dígitos y nada más,
+	 * y por tanto se puede transformar en una variable int o no.
+	 * 
+	 * @param entrada - String a validar
+	 * @return true si son solo dígitos, false en caso contrario
+	 */
+	public static boolean checkNum(String entrada) {
+		
+		boolean esCorrecto = true;
+		
+		entrada = entrada.trim();
+		
+		try {
+			Integer.parseInt(entrada);
+			
+		}catch(Exception e) {
+			esCorrecto = false;
+		}
+		
+		return (esCorrecto);
 	}
 		
 		
-		//pide un nº de opcion del menu mostrado previamente, valida si es una opcion dentro de las posibles o si es la de volver
-		//si es incorrecto, vuelve a pedir el dato
-		//cuando es correcto, devuelve la opcion que ha elegido y que es correcta
+	//pide un nº de opcion del menu mostrado previamente, valida si es una opcion dentro de las posibles o si es la de volver
+	//si es incorrecto, vuelve a pedir el dato
+	//cuando es correcto, devuelve la opcion que ha elegido y que es correcta
+		
+	/**
+	 * Este metodo gestiona la seleccion de una opcion mostrada previamente en un menu.
+	 * El usuario debe introducir un dato en respuesta de la peiticion, y debe ser una dentro de las
+	 * ofrecidas. El metodo recibe y muestra la peticion, y recibe el array de valores para poder 
+	 * verificar si la seleccion del usuario esta dentro del rango del tamaño del arraylist.	
+	 * @param <TipoDeArrayList> - indica que el arraylist es de cualquier tipo de objeto
+	 * @param peticion - lo que se pide al usuario
+	 * @param arrayObjetos - el arraylist de el objeto que sea
+	 * @return devuelve un entero que es indice de la opcion elegida dentro del arraylist recibido
+	 */
 	public static <TipoDeArrayList> int opcionCorrecta(String peticion, ArrayList<TipoDeArrayList> arrayObjetos) {
 		String				entrada = null;
 		int					seleccionIndice = 0;
